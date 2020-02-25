7f78e66924655aa655760ec065d528b8e7ca5ad8
Vincent Servio <vincentservio@gmail.com>
2020-02-25 04:03:08 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -74,7 +74,7 @@ end
 end
 
  def list_pets 
-   "I have #{self.dogs.count}dog(s), and #{self.cats.count} cat(s)."
+   "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
  
  end 
 end
